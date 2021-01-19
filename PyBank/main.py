f4e90935aecc4e3d2058f3858d391089634ba0d2
Daniela Marroquín <daniela.montufar18@gmail.com>
2021-01-19 13:21:44 -0600

    Adding final Results PyPoll and Complete Resources

--- PyBank/main.py
+++ PyBank/main.py
@@ -40,7 +40,8 @@ with open(csvpath) as csvfile:
         greatest_decrease = min(amount_change)
         
     #Print the results
-    #Resource formating: https://www.kite.com/python/answers/how-to-print-a-float-with-two-decimal-places-in-python#:~:text=Use%20str.,float%20with%20two%20decimal%20places&text=format(number)%20with%20%22%7B,number%20with%20two%20decimal%20places
+    #Resource formating: https://www.kite.com/python/answers/how-to-print-a-float-with-two-decimal-places-in-python#:~:text=Use%20str.,float%20with%20two%20decimal%20places&text=format(number)%20with%20%22%7B,number%20with%20two%20decimal%20places.
+    #Resource sys module: https://www.kite.com/python/answers/how-to-redirect-print-output-to-a-text-file-in-python#:~:text=Use%20sys.,output%20to%20a%20text%20file&text=stdout%20.,print%20output%20to%20the%20file.
     sys.stdout = open('pybank_results.txt', 'w')
     print(f'''
     Financial Analysis
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -42,10 +42,27 @@ with open(csvpath) as csvfile:
     li_percentage_votes = (li_votes/len(nr_votes))*100
     otooley_percentage_votes = (otooley_votes/len(nr_votes))*100
 
+    #List of percentage votes
+    percentage_votes = [khan_percentage_votes, correy_percentage_votes, li_percentage_votes, otooley_percentage_votes]
     
+    #Assign the winner based on popular vote
+    winner_vote = candidates[percentage_votes.index(max(percentage_votes))]
 
-         
+    #print and save results as txt format
+    #Resource formating: https://www.kite.com/python/answers/how-to-print-a-float-with-two-decimal-places-in-python#:~:text=Use%20str.,float%20with%20two%20decimal%20places&text=format(number)%20with%20%22%7B,number%20with%20two%20decimal%20places.
+    #Resource sys module: https://www.kite.com/python/answers/how-to-redirect-print-output-to-a-text-file-in-python#:~:text=Use%20sys.,output%20to%20a%20text%20file&text=stdout%20.,print%20output%20to%20the%20file.
+    sys.stdout = open('pypoll_results.txt', 'w')
+    print(f'''
+    Election Results
+    --------------------------
+    Total Votes: {len(nr_votes)}
+    --------------------------
+    {candidates[0]}: {"{:.3f}".format(khan_percentage_votes)}% ({khan_votes})
+    {candidates[1]}: {"{:.3f}".format(correy_percentage_votes)}% ({correy_votes})
+    {candidates[2]}: {"{:.3f}".format(li_percentage_votes)}% ({li_votes})
+    {candidates[3]}: {"{:.3f}".format(otooley_percentage_votes)}% ({otooley_votes})
+    --------------------------
+    Winner: {winner_vote}
+    ''')
 
-    print(len(nr_votes))
-    print(list(candidates))
-    print(khan_percentage_votes)
+    sys.stdout.close()
