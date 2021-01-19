c74ed5138967f14cddc7f67f02bb3d207c66796a
Daniela Marroquín <daniela.montufar18@gmail.com>
2021-01-18 18:05:50 -0600

    Changes calculation

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,7 +9,8 @@ csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
 
 #Defining Variables
 nr_months = 0
-total_amount = 0
+net_amount = []
+amount_change = []
 
 #Open the file in read mode and store it in te variable csvfile
 with open(csvpath) as csvfile:
@@ -18,13 +19,24 @@ with open(csvpath) as csvfile:
    
     for row in csvreader:
         
-        nr_months = nr_months + 1
-        total_amount += int(row[1])
+        #+= add and asign, Reference: https://medium.com/@rinu.gour123/different-types-of-operators-in-python-a2dde168f0a8
+        nr_months += 1
+        net_amount.append(int(row[1]))
+        
+    for amount in range(1, len(net_amount)):
+        amount_change.append(net_amount[amount] - net_amount[amount-1])
+        avg_change = sum(amount_change)/len(amount_change)
+        
+
+
+
 
 
     #Check the results
     #print(nr_months)
-    #print(total_amount)
+    #print(sum(net_amount))
+    #print(amount_change), Resource: https://www.kite.com/python/answers/how-to-print-a-float-with-two-decimal-places-in-python#:~:text=Use%20str.,float%20with%20two%20decimal%20places&text=format(number)%20with%20%22%7B,number%20with%20two%20decimal%20places.
+    #print("{:.2f}".format(avg_change))
 
 
     
